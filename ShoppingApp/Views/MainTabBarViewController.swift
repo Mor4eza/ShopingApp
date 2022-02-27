f94b5392998d8eb4ed1e7aebe621e435fdbba0f2
Morteza <morteza.ghrdi@gmail.com>
2022-02-27 14:00:02 +0330

    rearrange  project files

--- ShoppingApp/Views/MainTabBarViewController.swift
+++ ShoppingApp/Views/MainTabBarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarViewController.swift
 //  ShoppingApp
 //
 //  Created by Morteza on 2/27/22.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UITabBarController {
+class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         selectedIndex = 2
+        self.view.backgroundColor = UIColor(named: "VCBackColor")
     }
-
 
 }
 
